f09e716a0e9c67ce7c1654b2f23a9b0103a0caab
Caleb Wagner <caleb@finwaregroup.com>
2026-01-16 09:40:25 +0200

    render test

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
-const app = express()
 const cors = require('cors')
+const app = express()
 app.use(express.static('dist'))
-
 app.use(cors())
-
 app.use(morgan('tiny'))
 app.use(express.json())
 
